2c7859972f37753c274de8a51caa9f814c29925f
igorminash <iminash@inbox.ru>
2022-11-20 14:36:16 +0300

    Добавлено решение 10-ой Задачи

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+string number = Console.ReadLine()!;
+
+
+if (number.Length !=3)
+{
+    Console.WriteLine("Введено неверное число");
+}
+else
+{
+     Console.WriteLine(number[1]);
+}
