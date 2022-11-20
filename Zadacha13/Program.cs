149455d79263186b2216147872fdda714e55703c
igorminash <iminash@inbox.ru>
2022-11-20 15:03:47 +0300

    Добавлено решение 13-ой Задачи

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine()!;
+
+if (number.Length >= 6) Console.WriteLine("Нарушено условие задачи");
+
+if (number.Length <= 2) Console.WriteLine("Нет третьего числа");
+
+if (number.Length > 2 && number.Length < 6) Console.WriteLine(number[2]);
+    
